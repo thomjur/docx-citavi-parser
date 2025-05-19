2704c046e6a4fd6d690ab4d0c1de5824735ce671
raketen <pyristfeuer@gmail.com>
2025-05-19 12:18:37 +0200

    v0.0.1

---
+++ README.md
@@ -0,0 +1,32 @@
+# DOCX-CITAVI-PARSER
+
+This program parses a DOCX file that includes embedded citations using CITAVI's Word plugin. It compares the embedded citations with entries in a BibTeX file to find the matching citation keys/IDs. It then adds the keys in the form of a CiteProc citation used by Pandoc.
+
+For example:
+
+> ... (Krech, Elwert 2024, 2-6) ...
+
+=>
+
+> ... (Krech, Elwert 2024, 2-6) [@krech.2024, 2-6] ...
+
+The program can be used in typesetting pipelines and should facilitate creating a Markdown file with Pandoc, including a first version of CiteProc citations, since Pandoc can, as of today, only handle embedded citations created using the Zotero plugin for Word.
+
+## Usage
+You can download the Windows, Mac, or Linux executable or build your own version using `go build` in the main directory of this repository.
+
+The CLI program expects a DOCX file with CITAVI citations and a corresponding BibTeX file.
+
+Simply execute in the command line:
+
+``docx-citavi-parser <PATH-TO-WORD-FILE> <PATH-TO-BIBTEX-FILE>`
+
+The results are a `NEWDOCUMENT.xml` and `NEWFOOTNOTES.xml`, which include the CiteProc citations and should replace the former `document.xml` and `footnotes.xml` in the DOCX archive (in the subfolder word).
+
+Please note that finding the keys in BibTeX currently only works via title comparisons, which misses quite a few entries, particularly those with a lot of non-Latin characters or accentuations.
+
+## Versions
+
+### 0.0.1 (May 10, 2025)
+
+- initial version
--- main.go
+++ main.go
@@ -32,22 +32,27 @@ func (e *BibEntry) toCiteproc() string {
 }
 
 func main() {
-	// Open ZIP file
-	r, err := zip.OpenReader("Krech_Motak.docx")
+	// Parsing cli args
+	args := os.Args[1:]
+
+	if len(args) != 2 {
+		log.Fatalln("The DOCX Citavi Parser expects two arguments: (1) Path to DOCX file (2) Path to BibTeX file.")
+	}
+	// Open ZIP/DOCX file
+	r, err := zip.OpenReader(args[0])
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatalln(err)
 	}
 	defer r.Close()
 
 	// Parsing and loading BibTeX
 	bibliography, err := parseBibTeXFile("bib.bib")
 	if err != nil {
-		log.Fatalln("could not parse bibtex file")
+		log.Fatalln("Could not parse BibTeX file.")
 	}
-	bibliography.Info()
 
-	// Iterate over all files in ZIP archive
+	// Iterating over all files in ZIP archive
+	// To find document.xml and footnotes.xml to check for refs
 	for _, f := range r.File {
 		if f.Name == "word/document.xml" || f.Name == "word/footnotes.xml" {
 			fmt.Printf("Found: %s\n", f.Name)
@@ -56,76 +61,31 @@ func main() {
 				fmt.Println(err)
 				return
 			}
-			ParseXML(file, bibliography, f.Name)
+			parseXML(file, bibliography, f.Name)
 			defer file.Close()
 		}
 	}
 }
 
-func ParseXML(f io.Reader, bibliography *parser.BibTeXFile, fileName string) {
-	doc := etree.NewDocument()
-	citations := 0
-	if _, err := doc.ReadFrom(f); err != nil {
-		panic(err)
-	}
-	root := doc.Root()
-	fmt.Printf("Root Element: %s\n", root.Tag)
-	// Iterating over all sdt elements which are also used by Citavi
-	for _, p := range doc.FindElements("//sdt") {
-		// Finding instrText elements where bibliographical data is Base64 encoded
-		// Some Base64 encodings are split up into several instrText elements
-		var sb strings.Builder
-		for i, x := range p.FindElements(".//instrText") {
-			encodedString := x.Text()
-			// If first element does not start with ADDIN Citavi... we can skip
-			if !strings.HasPrefix(encodedString, "ADDIN CitaviPlaceholder") && i == 0 {
-				break
-			}
-			// we first need to remove the parts like ADDIN CITAVI etc.
-			// as well as the trailing } (if present)
-			if strings.HasPrefix(encodedString, "ADDIN CitaviPlaceholder") {
-				encodedString = strings.ReplaceAll(encodedString, "ADDIN CitaviPlaceholder{", "")
-			}
-			// Check if this is final part of string
-			if strings.HasSuffix(encodedString, "}") {
-				encodedString = strings.TrimSuffix(encodedString, "}")
-				sb.WriteString(encodedString)
-				break
-			}
-			sb.WriteString(encodedString)
-		}
+// Helper function to clean literature titles
+// to make them comparable between Citavi entries and BibTeX file
+// Issue is that BibTeX often includes transcriptions such as \"o for ö etc.}
+func cleanTitle(t string) string {
+	t = strings.ToLower(t)
+	// First, we replace all ä ö ü ß
+	t = strings.ReplaceAll(t, "ä", "a")
+	t = strings.ReplaceAll(t, "ö", "o")
+	t = strings.ReplaceAll(t, "ü", "u")
+	t = strings.ReplaceAll(t, "ß", "ss")
 
-		// Decoding Base64 encoding
-		decodedBytes, err := base64.StdEncoding.DecodeString(sb.String())
-		if err != nil {
-			fmt.Println("Error when decoding Base64:", err)
-			continue
-		}
-
-		bibEntryList, c, err := parseEntry(decodedBytes, bibliography)
-		if err != nil {
-			//fmt.Printf("%e", err)
-			continue
-		}
-		citations += c
-		// Creating string that should be added
-		contentString := createCitationString(bibEntryList)
-		// Create new element with this string in XML tree
-		textContainer := p.FindElement("./sdtContent//r/t")
-		if textContainer != nil {
-			textContainer.SetText(fmt.Sprintf("%s %s", textContainer.Text(), contentString))
-		}
-
-	}
-	fmt.Printf("We found %d citations.\n", citations)
-	// Writing new XML
-	if fileName == "word/document.xml" {
-		doc.WriteToFile("NEWDOC.xml")
-	} else {
-		doc.WriteToFile("NEWFN.xml")
-	}
+	// Removing all non alphanumerical signs
+	re := regexp.MustCompile(`\W`)
+	t = re.ReplaceAllString(t, "")
+	return t
 }
 
+// Helper function to create Citeproc citation like strings
+// e.g., [@<AUTHOR>, <PAGES>]
 func createCitationString(l []BibEntry) string {
 	var sb strings.Builder
 	for _, e := range l {
@@ -140,21 +100,64 @@ func createCitationString(l []BibEntry) string {
 
 }
 
-func writeToFileJSON(t []byte, i int) {
-	err := os.WriteFile(fmt.Sprintf("json%d.json", i), t, 0644)
-	if err != nil {
-		log.Fatal(err)
+// Function to find citation key in a BibTeX file based on the title of the
+// entry in Citavi embedded citation in DOCX file
+// TODO: Currently only matching complete titles (incl. subtitles) to find matching key
+func findCitationKey(bibEntry *BibEntry, bibliography *parser.BibTeXFile) error {
+	// Iterating over all entries in BibTeX file in hope to find a matching entry
+	found := false
+	cleanTitleBib := cleanTitle(bibEntry.Title)
+	for _, e := range bibliography.Entries {
+		if title, exists := e.Fields["title"]; exists {
+			cleanTitleEntry := cleanTitle(title)
+			if cleanTitleBib == cleanTitleEntry {
+				if e.Key != "" {
+					bibEntry.CitationKey = e.Key
+					found = true
+					break
+				}
+			}
+		}
+	}
+	if !found {
+		return errors.New(fmt.Sprintf("could not find any matching entries for %s in BibTeX file which is clean: %s", bibEntry.Title, cleanTitleBib))
+	} else {
+		return nil
 	}
 }
 
+// Function to parse BibTeX file using thomjur/verifybibtex/parser library
+func parseBibTeXFile(path string) (*parser.BibTeXFile, error) {
+	// Trying to open the file
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Something went terribly wrong :-(")
+	}
+	defer file.Close()
+
+	bibtexData, err := parser.ParseNewBibTeXFile(file)
+	if err != nil {
+		fmt.Println("Something went terribly wrong :-(")
+	}
+	// Don't forget to add filename afterwards
+	bibtexData.FilePath = path
+	return bibtexData, nil
+}
+
+// Function to parse a Citavi Citation entry in DOCX
+// Returns a list of BibEntry objects that are cited in this parseEntry plus the
+// number of citations found
 func parseEntry(e []byte, bibliography *parser.BibTeXFile) ([]BibEntry, int, error) {
 	var jsonData map[string]interface{}
 	var bibEntryList []BibEntry
 
+	// Since the Citavi JSON is too complex and I don't have a corresponding
+	// struct, I just parse certain paths in the JSON objects
+
 	// Unmarshalling first layer of Citavi JSON
 	err := json.Unmarshal(e, &jsonData)
 	if err != nil {
-		return []BibEntry{}, 0, errors.New("error shalling JSON")
+		return []BibEntry{}, 0, errors.New("Error unmarshalling JSON")
 	}
 
 	// Next is Entries list
@@ -228,59 +231,74 @@ func parseEntry(e []byte, bibliography *parser.BibTeXFile) ([]BibEntry, int, err
 	return bibEntryList, citations, nil
 }
 
-// Helper function to clean literature titles
-func cleanTitle(t string) string {
-	t = strings.ToLower(t)
-	// First, we replace all ä ö ü ß
-	t = strings.ReplaceAll(t, "ä", "a")
-	t = strings.ReplaceAll(t, "ö", "o")
-	t = strings.ReplaceAll(t, "ü", "u")
-	t = strings.ReplaceAll(t, "ß", "ss")
+// Main function to parse the document.xml or footnotes.xml
+// We are using the etree library here
+func parseXML(f io.Reader, bibliography *parser.BibTeXFile, fileName string) {
+	doc := etree.NewDocument()
+	citations := 0
+	if _, err := doc.ReadFrom(f); err != nil {
+		panic(err)
+	}
 
-	// Removing all non alphanumerical signs
-	re := regexp.MustCompile(`\W`)
-	t = re.ReplaceAllString(t, "")
-	return t
-}
-
-func findCitationKey(bibEntry *BibEntry, bibliography *parser.BibTeXFile) error {
-	// Iterating over all entries in BibTeX file in hope to find a matching entry
-	// TODO: Currently only comparing complete titles to find matching key
-	found := false
-	cleanTitleBib := cleanTitle(bibEntry.Title)
-	for _, e := range bibliography.Entries {
-		if title, exists := e.Fields["title"]; exists {
-			cleanTitleEntry := cleanTitle(title)
-			if cleanTitleBib == cleanTitleEntry {
-				if e.Key != "" {
-					bibEntry.CitationKey = e.Key
-					found = true
-					break
-				}
+	// Iterating over all sdt elements which are also used by Citavi
+	for _, p := range doc.FindElements("//sdt") {
+		// Finding instrText elements where bibliographical data is Base64 encoded
+		// Some Base64 encodings are split up into several instrText elements
+		var sb strings.Builder
+		for i, x := range p.FindElements(".//instrText") {
+			encodedString := x.Text()
+			// If first element does not start with ADDIN Citavi... we can skip
+			if !strings.HasPrefix(encodedString, "ADDIN CitaviPlaceholder") && i == 0 {
+				break
 			}
+			// we first need to remove the parts like ADDIN CITAVI etc.
+			// as well as the trailing } (if present)
+			if strings.HasPrefix(encodedString, "ADDIN CitaviPlaceholder") {
+				encodedString = strings.ReplaceAll(encodedString, "ADDIN CitaviPlaceholder{", "")
+			}
+			// Check if this is final part of string
+			if strings.HasSuffix(encodedString, "}") {
+				encodedString = strings.TrimSuffix(encodedString, "}")
+				sb.WriteString(encodedString)
+				break
+			}
+			sb.WriteString(encodedString)
+		}
+
+		// If no valid Base64 encoding in Citavi Entry was found, we can stop here
+		if sb.String() == "" {
+			continue
+		}
+
+		// Decoding Base64 encoding
+		decodedBytes, err := base64.StdEncoding.DecodeString(sb.String())
+		if err != nil {
+			fmt.Println("Error when decoding Base64:", err)
+			continue
+		}
+
+		// Parsing citations
+		bibEntryList, c, err := parseEntry(decodedBytes, bibliography)
+		if err != nil {
+			//fmt.Printf("%e", err)
+			continue
+		}
+		citations += c
+		// Creating string that should be added to the word file
+		contentString := createCitationString(bibEntryList)
+		// TODO: I don't know enough about Word XML schema, so we are adding the
+		// citeproc citations like [@AUTHOR, pages] behind the original in-text
+		// citation within the same w:t element
+		textContainer := p.FindElement("./sdtContent//r/t")
+		if textContainer != nil {
+			textContainer.SetText(fmt.Sprintf("%s %s", textContainer.Text(), contentString))
 		}
 	}
-	if !found {
-		return errors.New(fmt.Sprintf("could not find any matching entries for %s in BibTeX file which is clean: %s", bibEntry.Title, cleanTitleBib))
+	fmt.Printf("Found %d citations.\n", citations)
+	// Writing new XML
+	if fileName == "word/document.xml" {
+		doc.WriteToFile("NEWDOCUMENT.xml")
 	} else {
-		return nil
+		doc.WriteToFile("NEWFOOTNOTES.xml")
 	}
-}
-
-func parseBibTeXFile(path string) (*parser.BibTeXFile, error) {
-	BibTeXFilePath := "bib.bib"
-	// Trying to open the file
-	file, err := os.Open(path)
-	if err != nil {
-		fmt.Println("Something went terribly wrong :-(")
-	}
-	defer file.Close()
-
-	bibtexData, err2 := parser.ParseNewBibTeXFile(file)
-	if err2 != nil {
-		fmt.Println("Something went terribly wrong :-(")
-	}
-	// Don't forget to add filename afterwards
-	bibtexData.FilePath = BibTeXFilePath
-	return bibtexData, nil
 }
